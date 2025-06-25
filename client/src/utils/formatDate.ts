27280df88d18cb62ce9ce6f183f58f9f2694e03b
cptblackmore <redshatterr@gmail.com>
2025-06-25 14:53:04 +0300

    feat(client): add withoutTime option to formatDate

--- client/src/utils/formatDate.ts
+++ client/src/utils/formatDate.ts
@@ -1,9 +1,13 @@
-export function formatDate(iso: string): string {
+export function formatDate(iso: string, withoutTime = false): string {
   const date = new Date(iso);
 
   const day = date.getDate().toString().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear();
+
+  if (withoutTime) {
+    return `${day}.${month}.${year}`;
+  }
 
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
