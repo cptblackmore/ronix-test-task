3f5edcfed2cd2d414d90590a7e456ad19a62d2db
cptblackmore <redshatterr@gmail.com>
2025-06-25 23:22:00 +0300

    feat(server): create router

---
+++ server/src/router/index.ts
@@ -0,0 +1,20 @@
+import Router from "express";
+import multer from "multer";
+
+import {
+  createComment,
+  createPost,
+  getPostById,
+  getPosts,
+  likePost,
+} from "../controllers/post.controller";
+
+const upload = multer({ dest: "uploads/" });
+
+export const router = Router();
+
+router.get("/posts", getPosts);
+router.get("/posts/:postId", getPostById);
+router.post("/posts", upload.single("image"), createPost);
+router.post("/posts/:postId/comments", createComment);
+router.post("/posts/:postId/likes", likePost);
