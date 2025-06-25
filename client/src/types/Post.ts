bfdab09ab2ac49409d065ac43580846c3eb94c54
cptblackmore <redshatterr@gmail.com>
2025-06-25 14:29:55 +0300

    fix(client): correct Post type definition

--- client/src/types/Post.ts
+++ client/src/types/Post.ts
@@ -1,14 +1,10 @@
 import type { Comment } from "./Comment";
 
-type PostContent =
-  | { text: string; image?: string }
-  | { image: string; text?: string };
-
 export interface Post {
   id: number;
   postedAt: string;
   title: string;
-  content: PostContent;
   comments: Comment[];
   likes: number;
+  image?: string;
 }
