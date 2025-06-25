5ddfe050f1e194b049874277fd50ed18ba05c71c
cptblackmore <redshatterr@gmail.com>
2025-06-25 11:50:00 +0300

    feat(client): add initial PostCard component

---
+++ client/src/components/PostCard/PostCard.tsx
@@ -0,0 +1,29 @@
+import type { Post } from "../../types/Post";
+import { formatDate } from "../../utils/formatDate";
+
+interface PostCardProps {
+  post: Post;
+}
+
+export const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  return (
+    <article>
+      <header>
+        {"Author"} {formatDate(post.postedAt)}
+      </header>
+      <main>
+        <h2>{post.title}</h2>
+        {post.content.text ||
+          (post.content.image && (
+            <>
+              <p>{post.content.text}</p>
+              <img src={post.content.image} alt="" />
+            </>
+          ))}
+      </main>
+      <footer>
+        {post.likes} {post.comments.length}
+      </footer>
+    </article>
+  );
+};
